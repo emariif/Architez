083537f4ea69b389ed35bff4c155a507dbb614e4
Muhammad Arif Nurhuda <muhammad.arfnrhd@gmail.com>
2022-06-28 15:34:00 +0700

    responsive all

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,8 @@ const nav = document.querySelector("nav ul");
 menuToggle.addEventListener("click", function () {
   nav.classList.toggle("slide");
 });
+
+window.addEventListener("scroll", function () {
+  var navbar = document.querySelector("nav");
+  navbar.classList.toggle("scrolled", window.scrollY > 50);
+});
