893a0ea3d93b591fe69746e5f1e31b4d6c55f2f9
Muhammad Arif Nurhuda <muhammad.arfnrhd@gmail.com>
2022-06-29 11:22:42 +0700

    Scroll Animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 const menuToggle = document.querySelector(".menu-toggle #bars");
 const nav = document.querySelector("nav ul");
+window.addEventListener("scroll", show);
 
 menuToggle.addEventListener("click", function () {
   nav.classList.toggle("slide");
@@ -9,3 +10,19 @@ window.addEventListener("scroll", function () {
   var navbar = document.querySelector("nav");
   navbar.classList.toggle("scrolled", window.scrollY > 50);
 });
+
+function show() {
+  let elements = document.querySelectorAll(".elemen");
+
+  for (let i = 0; i < elements.length; i++) {
+    let topScreen = window.innerHeight;
+    let elementTopDistance = elements[i].getBoundingClientRect().top;
+    let scrollSize = 150;
+
+    if (elementTopDistance < topScreen - scrollSize) {
+      elements[i].classList.add("show");
+    } else {
+      elements[i].classList.remove("show");
+    }
+  }
+}
